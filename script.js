419b943202e1ca94a923ed08d18e2f704f320f30
Dipp3r <aruneesh4@gmail.com>
2023-12-14 19:28:46 +0530

    handled: cell click

--- script.js
+++ script.js
@@ -73,3 +73,20 @@ function handleResultValidation() {
 
     handlePlayerChange();
 }
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if(gameState[clickedCellIndex] !== "" || !gameActive)
+        return;
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
