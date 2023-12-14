984eff2e85e98031acfbea4e8eb31fb7a5911a00
Dipp3r <aruneesh4@gmail.com>
2023-12-14 19:27:53 +0530

    add: winningMsg handle: result valdation check

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ function handlePlayerChange() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     statusDisplay.innerHTML = currentPlayerTurn();
 }
+
+function winningMessage(){
+    if(currentPlayer == "X"){
+        currentXScore+=1;
+    }
+    else{
+       currentYScore+=1;
+    }
+    return `Player ${currentPlayer} has won!🎯`;
+};
+
+function handleResultValidation() {
+    let roundWon = false;
+    for(let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = gameState[winCondition[0]];
+        const b = gameState[winCondition[1]];
+        const c = gameState[winCondition[2]];
+        if(a === '' || b === '' || c === '')
+            continue;
+        if(a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if(roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    const roundDraw = !gameState.includes("");
+    if(roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
